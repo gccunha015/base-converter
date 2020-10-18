aeada6af501bf05d656bf10256f59855a2631e7f
gccunha015 <gccunha015@gmail.com>
2020-10-17 22:03:40 -0300

    design changes and refactoring

--- index.js
+++ index.js
@@ -1,39 +1,89 @@
-function readInput() {
-  const input = document.querySelector(".binary-input");
-  const valid = input.value.match(new RegExp("^[0-1]+$")) ? true : false;
-  const value = valid ? input.value : null;
+function convertToDecimal(event) {
+  const target = event.target;
+  const value = convertFromBinaryToDecimal(target.value);
+  const element = selectOneElement(".decimal-input");
+
+  element.value = value;
+}
+
+function convertToBinary(event) {
+  const target = event.target;
+  const value = target.value;
+  const element = selectOneElement(".binary-input");
+
+  element.value = value;
+}
+
+function showResult() {
+  const binary = readInput();
+  const decimal = convertFromBinaryToDecimal(binary);
   
-  if (!valid) alert("You can only input 0s and 1s");
+  if (wasConverted(decimal)) showConvertedValue(decimal);
+  else showInvalidInput();
+}
+
+function showConvertedValue(decimal) {
+  const elementResult = selectOneElement("#result");
+  
+  elementResult.firstElementChild
+    .innerHTML = decimal;
+
+  elementResult.hidden = false;
+}
+
+function showInvalidInput() {
+  alert("You can only input 0s and 1s");
+}
+
+function wasConverted(decimal) {
+  return decimal !== null;
+}
+
+function convertFromBinaryToDecimal(binary) {
+  let decimal = nullIfNullElseZero(binary);
+
+  if (binary) decimal = convert(binary);
+
+  return decimal;
+}
+
+function convert(binary) {
+  let decimal = 0;
+  const binaryLastIndex = binary.length - 1;
+
+  for (let i=0; i<=binaryLastIndex ; i++) {
+    decimal += binary[i] * Math.pow(2, binaryLastIndex - i);
+  }
+
+  return decimal;
+}
+
+function nullIfNullElseZero(value) {
+  return value === null ? null : 0;
+}
+
+function readInput() {
+  const input = selectOneElement(".binary-input");
+  const value = validateInputValue(input);
   
   return value;
 }
 
-function binaryToDecimal(binary) {
-  let decimal = binary === null ? null : 0;
+function validateInputValue(input) {
+  let value = input.value;
+  const valid = isBinary(value);
 
-  if (binary) {
-    const binaryLastIndex = binary.length - 1;
-  
-    for (let i=0; i <= binaryLastIndex; i++) {
-      decimal += binary[i] * Math.pow(2, binaryLastIndex - i);
-    }
-  
-  }
+  return valueIfValidElseNull(value, valid);
+}
 
-  return decimal;
-};
+function valueIfValidElseNull(value, valid) {
+  return valid ? value : null;
+}
 
-function showResult() {
-  const value = binaryToDecimal(readInput());
-  
-  if (value !== null) {
-    const elementResult = document.querySelector("#result");
-  
-    elementResult.firstElementChild
-      .innerHTML = value;
-  
-    elementResult.hidden = false;
-  }
-};
+function selectOneElement(description) {
+  return document.querySelector(description);
+}
 
-document.querySelector(".binary-convert").onclick = showResult;
+function isBinary(value) {
+  return value.match(new RegExp("^[0-1]+$")) ? true : false;
+}
